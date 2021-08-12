5021c5a2bdd99245fc384242aa89051969c0c7f4
José Loarca <joseloarca97@icloud.com>
2021-08-11 22:30:11 -0600

    test basic node api using ts

---
+++ dist/routes/todos.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+let todos = [];
+const router = express_1.Router();
+router.get('/', (req, res, next) => {
+    return res
+        .status(200)
+        .json({ todos: todos });
+});
+router.post('/todo', (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+    todos.push(newTodo);
+    return res
+        .status(201)
+        .json({ message: 'Added todo', todo: newTodo, todos: todos });
+});
+router.put('/todo/:todoId', (req, res, next) => {
+    const tid = req.params.todoId;
+    const todoIndex = todos.findIndex(todoItem => todoItem.id === tid);
+    if (todoIndex >= 0) {
+        todos[todoIndex] = { id: tid, text: req.body.text };
+        return res
+            .status(200)
+            .json({ message: 'Updated todo', todos: todos });
+    }
+    return res
+        .status(404)
+        .json({ message: 'Could not find todo!' });
+});
+router.delete('/todo/:todoId', (req, res, next) => {
+    todos = todos.filter(todoItem => todoItem.id !== req.params.todoId);
+    return res
+        .status(200)
+        .json({ message: 'Deleted todo', todos: todos });
+});
+exports.default = router;
